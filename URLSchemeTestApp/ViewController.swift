372d11b1cc2577a749cb20128bce586b80efab32
Sahan Nagodavithana <sahan.nagodavithana@gmail.com>
2022-09-23 14:42:02 +0530

    Init

--- URLSchemeTestApp/ViewController.swift
+++ URLSchemeTestApp/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func open(_ sender: Any) {
+        let url = URL(string: "SoHive:Meeting?id=369&source=TestApp")
 
+        UIApplication.shared.open(url!) { (result) in
+            if result {
+               // The URL was delivered successfully!
+            }else{
+                let url = URL(string: "https://apps.apple.com/lk/app/sohive/id1596274003")
+                UIApplication.shared.open(url!) { (result) in
+                    if result {
+                       // The URL was delivered successfully!
+                    }else{
+                        
+                    }
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
